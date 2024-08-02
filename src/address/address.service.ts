b83bb19810b00d8e12251d00645f85ef0d43c7b5
luthfyhakim <luthfyhakim250404@gmail.com>
2024-08-02 22:42:34 +0700

    feat(address): add API create, get, update, delete, list address

---
+++ src/address/address.service.ts
@@ -0,0 +1,228 @@
+import { HttpException, Inject, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { PrismaService } from 'src/common/prisma.service';
+import { ValidationService } from 'src/common/validation.service';
+import {
+  AddressResponse,
+  CreateAddressRequest,
+  DeleteAddressRequest,
+  GetAddressRequest,
+  UpdateAddressRequest,
+} from 'src/model/address.model';
+import { Logger } from 'winston';
+import { AddressValidation } from './address.validation';
+
+@Injectable()
+export class AddressService {
+  constructor(
+    private prismaService: PrismaService,
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+    private validationService: ValidationService,
+  ) {}
+
+  async create(
+    user: User,
+    request: CreateAddressRequest,
+  ): Promise<AddressResponse> {
+    // validasi request yang masuk
+    const createRequest: CreateAddressRequest = this.validationService.validate(
+      AddressValidation.CREATE,
+      request,
+    );
+
+    // cek kontak ada atau tidak
+    const contact = await this.prismaService.contact.findFirst({
+      where: {
+        id: createRequest.contact_id,
+        username: user.username,
+      },
+    });
+
+    if (!contact) {
+      throw new HttpException('Contact not found', 404);
+    }
+
+    // insert atau create new contact
+    const address = await this.prismaService.address.create({
+      data: createRequest,
+    });
+
+    return {
+      id: address.id,
+      street: address.street,
+      city: address.city,
+      province: address.province,
+      country: address.country,
+      postal_code: address.postal_code,
+    };
+  }
+
+  async get(user: User, request: GetAddressRequest): Promise<AddressResponse> {
+    // validasi request yang masuk
+    const getRequest: GetAddressRequest = this.validationService.validate(
+      AddressValidation.GET,
+      request,
+    );
+
+    // cek kontak ada atau tidak
+    const contact = await this.prismaService.contact.findFirst({
+      where: {
+        id: getRequest.contact_id,
+        username: user.username,
+      },
+    });
+
+    if (!contact) {
+      throw new HttpException('Contact not found', 404);
+    }
+
+    // cek alamat ada atau tidak
+    const address = await this.prismaService.address.findFirst({
+      where: {
+        id: getRequest.address_id,
+        contact_id: getRequest.contact_id,
+      },
+    });
+
+    if (!address) {
+      throw new HttpException('Address not found', 404);
+    }
+
+    return {
+      id: address.id,
+      street: address.street,
+      city: address.city,
+      province: address.province,
+      country: address.country,
+      postal_code: address.postal_code,
+    };
+  }
+
+  async update(
+    user: User,
+    request: UpdateAddressRequest,
+  ): Promise<AddressResponse> {
+    // validasi request yang masuk
+    const updateRequest: UpdateAddressRequest = this.validationService.validate(
+      AddressValidation.UPDATE,
+      request,
+    );
+
+    // cek kontak ada atau tidak
+    const contact = await this.prismaService.contact.findFirst({
+      where: {
+        id: updateRequest.contact_id,
+        username: user.username,
+      },
+    });
+
+    if (!contact) {
+      throw new HttpException('Contact not found', 404);
+    }
+
+    let address = await this.prismaService.address.findFirst({
+      where: {
+        id: updateRequest.id,
+        contact_id: updateRequest.contact_id,
+      },
+    });
+
+    // insert atau create new contact
+    address = await this.prismaService.address.update({
+      where: {
+        id: updateRequest.id,
+        contact_id: updateRequest.contact_id,
+      },
+      data: updateRequest,
+    });
+
+    return {
+      id: address.id,
+      street: address.street,
+      city: address.city,
+      province: address.province,
+      country: address.country,
+      postal_code: address.postal_code,
+    };
+  }
+
+  async delete(
+    user: User,
+    request: DeleteAddressRequest,
+  ): Promise<AddressResponse> {
+    // validasi request yang masuk
+    const deleteRequest: DeleteAddressRequest = this.validationService.validate(
+      AddressValidation.DELETE,
+      request,
+    );
+
+    // cek kontak ada atau tidak
+    const contact = await this.prismaService.contact.findFirst({
+      where: {
+        id: deleteRequest.contact_id,
+        username: user.username,
+      },
+    });
+
+    if (!contact) {
+      throw new HttpException('Contact not found', 404);
+    }
+
+    // cek alamat ada atau tidak
+    let address = await this.prismaService.address.findFirst({
+      where: {
+        id: deleteRequest.address_id,
+        contact_id: deleteRequest.contact_id,
+      },
+    });
+
+    if (!address) {
+      throw new HttpException('Address not found', 404);
+    }
+
+    address = await this.prismaService.address.delete({
+      where: {
+        id: deleteRequest.address_id,
+        contact_id: deleteRequest.contact_id,
+      },
+    });
+
+    return {
+      id: address.id,
+      street: address.street,
+      city: address.city,
+      province: address.province,
+      country: address.country,
+      postal_code: address.postal_code,
+    };
+  }
+
+  async list(user: User, contactId: number): Promise<AddressResponse[]> {
+    const contact = await this.prismaService.contact.findFirst({
+      where: {
+        id: contactId,
+        username: user.username,
+      },
+    });
+
+    if (!contact) {
+      throw new HttpException('Contact not found', 404);
+    }
+
+    const addresses = await this.prismaService.address.findMany({
+      where: {
+        contact_id: contactId,
+      },
+    });
+
+    return addresses.map((address) => ({
+      id: address.id,
+      street: address.street,
+      city: address.city,
+      province: address.province,
+      country: address.country,
+      postal_code: address.postal_code,
+    }));
+  }
+}
--- test/test.service.ts
+++ test/test.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../src/common/prisma.service';
 import * as bcrypt from 'bcrypt';
-import { User } from '@prisma/client';
-import { ContactResponse } from '../src/model/contact.model';
+import { Address, Contact, User } from '@prisma/client';
 
 @Injectable()
 export class TestService {
@@ -35,7 +34,7 @@ export class TestService {
     });
   }
 
-  async getContact(): Promise<ContactResponse> {
+  async getContact(): Promise<Contact> {
     return await this.prismaService.contact.findFirst({
       where: {
         username: 'test',
@@ -59,6 +58,40 @@ export class TestService {
     await this.prismaService.contact.deleteMany({
       where: {
         username: 'test',
+      },
+    });
+  }
+
+  async getAddress(): Promise<Address> {
+    return await this.prismaService.address.findFirst({
+      where: {
+        contact: {
+          username: 'test',
+        },
+      },
+    });
+  }
+
+  async createAddress() {
+    const contact = await this.getContact();
+    await this.prismaService.address.create({
+      data: {
+        contact_id: contact.id,
+        street: 'jalan test',
+        city: 'kota test',
+        province: 'provinsi test',
+        country: 'negara test',
+        postal_code: '1111',
+      },
+    });
+  }
+
+  async deleteAddress() {
+    await this.prismaService.address.deleteMany({
+      where: {
+        contact: {
+          username: 'test',
+        },
       },
     });
   }
